70ea85d01090ab5bafc705ff4359c192bc13ba4a
Shaan Patel <shaanpatel0598@gmail.com>
2021-10-07 11:06:07 -0500

    Disable eslint in two cases where rules may be broken
    vue.config.js uses CommonJS modules to export, but this is fine.
    Using process.env is also fine since the Vue build process replaces
    these at build time (although I should try and limit the usage in my
    code by declaring constants instead of using process.env all over the
    place).

--- src/auth/constants.js
+++ src/auth/constants.js
@@ -1,6 +1,7 @@
 export const CLIENT_ID = '2b467d30365e4954b54897641453a192'
 export const SCOPES = 'streaming user-modify-playback-state user-read-email user-read-private'
 
+// eslint-disable-next-line
 export const REDIRECT_URI = process.env.VUE_APP_REDIRECT_URI
 export const AUTHORIZATION_URL = 'https://accounts.spotify.com/authorize'
 export const TOKEN_URL = 'https://accounts.spotify.com/api/token'
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 module.exports = {
     transpileDependencies: [
         'vuetify'
